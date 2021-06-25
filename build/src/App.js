a467bcdda6e511b9e118b54d2b4454d038ccedc4
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-24 20:18:22 -0400

    hashrouter change

--- build/src/App.js
+++ build/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { HashRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Home from './pages/Home.js';
